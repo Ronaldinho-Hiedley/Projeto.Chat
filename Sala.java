a49dacd114fb70eadeb8528626862e82fc089965
Unita <rhcp1@aluno.ifal.edu.br>
2023-07-28 16:52:42 -0300

    Usando o gitgit add *

---
+++ Sala.java
@@ -0,0 +1,33 @@
+package chatWeb;
+
+import java.util.ArrayList;
+
+public class Sala {
+	private String nome;
+	private ArrayList<Usuario> usuarios;
+	private ArrayList<Mensagem> mensagens;
+
+	public Sala(String nome) {// Cria e Inicializa objetos
+		this.nome = nome;
+		usuarios  = new ArrayList<Usuario>();
+		mensagens = new ArrayList<Mensagem>();
+	}
+
+	public void addUsuario(Usuario usuario) {
+		usuarios.add(usuario);
+		System.out.println("Adicionou usuario.");
+	}
+	
+	public void enviarMSG(Mensagem msg) {
+		mensagens.add(msg);
+		System.out.println("Enviou Mensagem");
+	}
+
+	public String getNome() {
+		return nome;
+	}
+package chatWeb;
+
+public class Sala {
+
+}
---
+++ Teste.java
@@ -0,0 +1,29 @@
+package chatWeb;
+
+public class Teste {
+
+	
+	public static void main(String[] args) {
+		
+
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+Teste.java
+Exibindo Teste.java…package chatWeb;
+
+public class Teste {
+
+}
